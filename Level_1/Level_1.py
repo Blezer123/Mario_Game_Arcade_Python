0ea9a66f3ebf0809b0ab49b3be05afb35ad93d98
artemblazer12 <blazer_2024@mail.ru>
2026-01-21 21:24:15 +0300

    Add files via upload
    tmx

--- Level_1/Level_1.py
+++ Level_1/Level_1.py
@@ -1,5 +1,4 @@
 import arcade
-import os
 from Main.main import Mario
 
 SPEED = 5
@@ -13,10 +12,7 @@ class Level_1(arcade.Window):
     def __init__(self):
         super().__init__(1000, 600, "Mario Game", fullscreen=True)
 
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-
-        tmx_path = os.path.join(current_dir, "..", "Level_1", "Level_1.tmx")
-        tile_map = arcade.load_tilemap(tmx_path, scaling=1)
+        tile_map = arcade.load_tilemap("C:\\2_Artema\\Level_1\\Level_1.tmx", scaling=2)
 
         self.map_pixel_width = tile_map.width * tile_map.tile_width
         self.map_pixel_height = tile_map.height * tile_map.tile_height
@@ -33,15 +29,61 @@ class Level_1(arcade.Window):
         self.animation_timer = 0
         self.current_texture = 0
 
-        images_dir = os.path.join(current_dir, "..", "images")
-        self.textures = [[arcade.load_texture(os.path.join(images_dir, "Grib_1.png")),
-                          arcade.load_texture(os.path.join(images_dir, "Grib_2.png"))]]
+        self.textures = [[arcade.load_texture("C:\\2_Artema\\images\\Grib_1.png"), arcade.load_texture("C:\\2_Artema\\images\\Grib_2.png")]]
 
     def setup(self):
-        Mario.setup(self)
+        tile_map = arcade.load_tilemap("C:\\2_Artema\\Level_3\\Level_3.tmx", scaling=1)
+
+        self.Ground = tile_map.sprite_lists["Ground"]
+        self.Sky = tile_map.sprite_lists["Sky"]
+        self.Coins = tile_map.sprite_lists["Coins"]
+        self.secret_blocks_grib_life = tile_map.sprite_lists["secret_blocks_grib_life"]
+        self.Mob_Grib = tile_map.sprite_lists["Mob_Grib"]
+        self.secret_blocks_grib_baff = tile_map.sprite_lists["secret_blocks_grib_baff"]
+        self.BG = tile_map.sprite_lists["BackGround"]
+        self.Truba = tile_map.sprite_lists["Truba"]
+        self.Mob_Turtle = tile_map.sprite_lists["Mob_Turtle"]
+
+        self.grid = [[0] * 150 for x in range(50)]
+
+        self.player = arcade.Sprite(self.player_texture, scale=1)
+        y = 2 * self.cell_size + self.cell_size // 2
+        x = 2 * self.cell_size + self.cell_size // 2
+        self.player.center_x = 64
+        self.player.center_y = 9 * 64
+        self.all_sprites.append(self.player)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player,
+            platforms=self.Ground,
+            gravity_constant=GRAVITY
+        )
+
+        arcade.set_background_color(arcade.color.SKY_BLUE)
+
+        for enemy in self.Mob_Grib:
+            enemy.patrol_distance = 0
+            enemy.direction = 1
+            enemy.speed = ENEMY_SPEED
 
     def on_draw(self):
-        Mario.on_draw(self)
+        self.clear()
+
+        self.world_camera.use()
+
+        self.BG.draw()
+        self.Ground.draw()
+        self.Sky.draw()
+        self.Truba.draw()
+        self.secret_blocks_grib_baff.draw()
+        self.secret_blocks_grib_life.draw()
+        self.Mob_Grib.draw()
+        self.Mob_Turtle.draw()
+        self.Coins.draw()
+
+        self.all_sprites.draw()
+
+        self.gui_camera.use()
 
     def on_update(self, delta_time: float):
         Mario.on_update(self, delta_time)
