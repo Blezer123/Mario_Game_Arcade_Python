c13807c333694a75012a8c3df36fda621134d4d3
Blazer3D <157282406+Blezer123@users.noreply.github.com>
2026-01-21 17:21:41 +0300

    Обновление 1.8

--- Level_3/Level_3.py
+++ Level_3/Level_3.py
@@ -1,4 +1,5 @@
 import arcade
+import os
 from Main.main import Mario
 
 SPEED = 5
@@ -12,7 +13,10 @@ class Level_3(arcade.Window):
     def __init__(self):
         super().__init__(1000, 600, "Mario Game", fullscreen=True)
 
-        tile_map = arcade.load_tilemap("C:\\2_Artema\\Level_3\\Level_3.tmx", scaling=2)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+        tmx_path = os.path.join(current_dir, "..", "Level_3", "Level_3.tmx")
+        tile_map = arcade.load_tilemap(tmx_path, scaling=1)
 
         self.map_pixel_width = tile_map.width * tile_map.tile_width
         self.map_pixel_height = tile_map.height * tile_map.tile_height
@@ -29,151 +33,24 @@ class Level_3(arcade.Window):
         self.animation_timer = 0
         self.current_texture = 0
 
-        self.textures = [[arcade.load_texture("C:\\2_Artema\\images\\Grib_1.png"), arcade.load_texture("C:\\2_Artema\\images\\Grib_2.png")]]
+        images_dir = os.path.join(current_dir, "..", "images")
+        self.textures = [[arcade.load_texture(os.path.join(images_dir, "Grib_1.png")),
+                          arcade.load_texture(os.path.join(images_dir, "Grib_2.png"))]]
 
     def setup(self):
-        tile_map = arcade.load_tilemap("C:\\2_Artema\\Level_3\\Level_3.tmx", scaling=1)
-
-        self.Ground = tile_map.sprite_lists["Ground"]
-        self.Sky = tile_map.sprite_lists["Sky"]
-        self.Coins = tile_map.sprite_lists["Coins"]
-        self.secret_blocks_grib_life = tile_map.sprite_lists["secret_blocks_grib_life"]
-        self.Mob_Grib = tile_map.sprite_lists["Mob_Grib"]
-        self.secret_blocks_grib_baff = tile_map.sprite_lists["secret_blocks_grib_baff"]
-        self.BG = tile_map.sprite_lists["BackGround"]
-        self.Truba = tile_map.sprite_lists["Truba"]
-        self.Mob_Turtle = tile_map.sprite_lists["Mob_Turtle_Red"]
-
-        self.grid = [[0] * 150 for x in range(50)]
-
-        self.player = arcade.Sprite(self.player_texture, scale=1)
-        y = 2 * self.cell_size + self.cell_size // 2
-        x = 2 * self.cell_size + self.cell_size // 2
-        self.player.center_x = 64
-        self.player.center_y = 9 * 64
-        self.all_sprites.append(self.player)
-
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player,
-            platforms=self.Ground,
-            gravity_constant=GRAVITY
-        )
-
-        arcade.set_background_color(arcade.color.SKY_BLUE)
-
-        for enemy in self.Mob_Grib:
-            enemy.patrol_distance = 0
-            enemy.direction = 1
-            enemy.speed = ENEMY_SPEED
+        Mario.setup(self)
 
     def on_draw(self):
-        self.clear()
-
-        self.world_camera.use()
-
-        self.BG.draw()
-        self.Ground.draw()
-        self.Sky.draw()
-        self.Truba.draw()
-        self.secret_blocks_grib_baff.draw()
-        self.secret_blocks_grib_life.draw()
-        self.Mob_Grib.draw()
-        self.Mob_Turtle.draw()
-        self.Coins.draw()
-
-        self.all_sprites.draw()
-
-        self.gui_camera.use()
+        Mario.on_draw(self)
 
     def on_update(self, delta_time: float):
-        self.physics_engine.update()
-
-        coins_hit_list = arcade.check_for_collision_with_list(self.player, self.Coins)
-        for coin in coins_hit_list:
-            coin.remove_from_sprite_lists()
-
-        enemy_hit_list = arcade.check_for_collision_with_list(self.player, self.Mob_Grib)
-        enemy_hit_list += arcade.check_for_collision_with_list(self.player, self.Mob_Turtle)
-
-        if enemy_hit_list:
-            pass
-
-        secret_hit_list = arcade.check_for_collision_with_list(self.player, self.secret_blocks_grib_life)
-        secret_hit_list += arcade.check_for_collision_with_list(self.player, self.secret_blocks_grib_baff)
-
-        if self.player.center_x < 0:
-            self.player.center_x = 0
-            self.player.change_x = 0
-
-        if self.player.center_x > self.map_pixel_width:
-            self.player.center_x = self.map_pixel_width
-            self.player.change_x = 0
-
-        cam_x, cam_y = self.world_camera.position
-        dz_left = cam_x - self.DEAD_ZONE_W // 2
-        dz_right = cam_x + self.DEAD_ZONE_W // 2
-        dz_bottom = cam_y - self.DEAD_ZONE_H // 2
-        dz_top = cam_y + self.DEAD_ZONE_H // 2
-
-        px, py = self.player.center_x, self.player.center_y
-        target_x, target_y = cam_x, cam_y
-
-        if px < dz_left:
-            target_x = px + self.DEAD_ZONE_W // 2
-        elif px > dz_right:
-            target_x = px - self.DEAD_ZONE_W // 2
-        if py < dz_bottom:
-            target_y = py + self.DEAD_ZONE_H // 2
-        elif py > dz_top:
-            target_y = py - self.DEAD_ZONE_H // 2
-
-        half_w = self.world_camera.viewport_width / 2
-        half_h = self.world_camera.viewport_height / 2
-        target_x = max(half_w, min(self.map_pixel_width - half_w, target_x))
-        target_y = max(half_h, min(self.map_pixel_height - half_h, target_y))
-
-        smooth_x = (1 - CAMERA_LERP) * cam_x + CAMERA_LERP * target_x
-        smooth_y = (1 - CAMERA_LERP) * cam_y + CAMERA_LERP * target_y
-
-        self.cam_target = (smooth_x, smooth_y)
-        self.world_camera.position = (self.cam_target[0], self.cam_target[1])
-
-        for enemy in self.Mob_Grib:
-            enemy.change_x = enemy.speed * enemy.direction
-            enemy.patrol_distance += enemy.change_x
-
-            if enemy.patrol_distance >= 100:
-                enemy.direction = -1
-            elif enemy.patrol_distance <= -100:
-                enemy.direction = 1
-
-            enemy.center_x += enemy.change_x
-
-            enemy.texture = (self.textures[0][self.current_texture])
-
-            self.animation_timer += 1
-            if self.animation_timer == 25:
-                self.current_texture = 1 - self.current_texture
-                self.animation_timer = 0
+        Mario.on_update(self, delta_time)
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.player.change_y = PLAYER_JUMP_SPEED
-        elif key == arcade.key.DOWN:
-            pass
-        elif key == arcade.key.LEFT:
-            self.player.change_x = -SPEED
-        elif key == arcade.key.RIGHT:
-            self.player.change_x = SPEED
+        Mario.on_key_press(self, key, modifiers)
 
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            if self.player.change_x < 0:
-                self.player.change_x = 0
-        elif key == arcade.key.RIGHT:
-            if self.player.change_x > 0:
-                self.player.change_x = 0
+        Mario.on_key_release(self, key, modifiers)
 
 
 def main():
